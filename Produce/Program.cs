271179f8188f189dbc6749097e832e646f9a60b6
NaanKaran <karankaruna.mca@gmail.com>
2022-04-26 11:21:22 +0530

    Initial commit

---
+++ Produce/Program.cs
@@ -0,0 +1,224 @@
+﻿// Copyright 2020 Confluent Inc.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace CCloud
+{
+    class Program
+    {
+        static async Task<ClientConfig> LoadConfig(string configPath, string certDir)
+        {
+            try
+            {
+                //var cloudConfig = (await File.ReadAllLinesAsync(configPath));
+
+                //    var yy = cloudConfig
+                //    .Where(line => !line.StartsWith("#"))
+                //    .ToDictionary(
+                //        line => line.Substring(0, line.IndexOf('=')),
+                //        line => line.Substring(line.IndexOf('=') + 1));
+
+                    var config = new Dictionary<string, string>()
+                    {
+                        {"bootstrap.servers","pkc-7prvp.centralindia.azure.confluent.cloud:9092"},
+                        {"security.protocol","SASL_SSL"},
+                        {"sasl.mechanisms","PLAIN"},
+                        {"sasl.username","RYJKYBO7WWKK3DPE"},
+                        {"sasl.password","Sjmme1z6evvg5Ow2npHEybQYHAf04hjMm3W66RT2SLczAiaU0sAzwb+uLvFpGBZN"},
+                        {"session.timeout.ms","45000"}
+                    };
+                    
+                var clientConfig = new ClientConfig(config);
+
+                if (certDir != null)
+                {
+                    clientConfig.SslCaLocation = certDir;
+                }
+
+                return clientConfig;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occured reading the config file from '{configPath}': {e.Message}");
+                System.Environment.Exit(1);
+                return null; // avoid not-all-paths-return-value compiler error.
+            }
+        }
+
+        static async Task CreateTopicMaybe(string name, int numPartitions, short replicationFactor, ClientConfig cloudConfig)
+        {
+            using (var adminClient = new AdminClientBuilder(cloudConfig).Build())
+            {
+                try
+                {
+                    await adminClient.CreateTopicsAsync(new List<TopicSpecification> {
+                        new TopicSpecification { Name = name, NumPartitions = numPartitions, ReplicationFactor = replicationFactor } });
+                }
+                catch (CreateTopicsException e)
+                {
+                    if (e.Results[0].Error.Code != ErrorCode.TopicAlreadyExists)
+                    {
+                        Console.WriteLine($"An error occured creating topic {name}: {e.Results[0].Error.Reason}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Topic already exists");
+                    }
+                }
+            }
+        }
+        
+        static void Produce(string topic, ClientConfig config)
+        {
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                int numProduced = 0;
+                int numMessages = 10;
+                for (int i=0; i<numMessages; ++i)
+                {
+                    var key = "alice";
+                    var val = JObject.FromObject(new { count = i }).ToString(Formatting.None);
+
+                    Console.WriteLine($"Producing record: {key} {val}");
+
+                    producer.Produce(topic, new Message<string, string> { Key = key, Value = val },
+                        (deliveryReport) =>
+                        {
+                            if (deliveryReport.Error.Code != ErrorCode.NoError)
+                            {
+                                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Produced message to: {deliveryReport.TopicPartitionOffset}");
+                                numProduced += 1;
+                            }
+                        });
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(10));
+
+                Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+            }
+        }
+
+        static void Consume(string topic, ClientConfig config)
+        {
+            var consumerConfig = new ConsumerConfig(config);
+            consumerConfig.GroupId = "dotnet-example-group-1";
+            consumerConfig.AutoOffsetReset = AutoOffsetReset.Latest;
+            consumerConfig.EnableAutoCommit = false;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build())
+            {
+                consumer.Subscribe(topic);
+                var totalCount = 0;
+                try
+                {
+                    while (true)
+                    {
+                        var cr = consumer.Consume(cts.Token);
+                       // totalCount += JObject.Parse(cr.Message.Value).Value<int>("count");
+                        Console.WriteLine($"Consumed record with key {cr.Message.Key} and value {cr.Message.Value}, and updated total count to");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ctrl-C was pressed.
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("usage: .. produce|consume <topic> <configPath> [<certDir>]");
+            System.Environment.Exit(1);
+        }
+
+        static async Task Main(string[] args)
+        {
+
+            //var config = new ProducerConfig
+            //{
+            //    BootstrapServers = "pkc-7prvp.centralindia.azure.confluent.cloud:9092",
+            //    SslCaLocation = @"C:\Users\Karunakaran\ssl\cacert.pem",
+            //    SecurityProtocol = SecurityProtocol.SaslSsl,
+            //    SaslMechanism = SaslMechanism.ScramSha256,
+            //    SaslUsername = "52CRQWGNBGNCISMI",
+            //    SaslPassword = "WNhdFWptVat6839cDsk5TRPWTXBZ5VvsaLxO5R0MgemuPo4VikXk0jbaqkAzA7W8",
+            //};
+
+            //// If serializers are not specified, default serializers from
+            //// `Confluent.Kafka.Serializers` will be automatically used where
+            //// available. Note: by default strings are encoded as UTF8.
+            //using (var p = new ProducerBuilder<Null, string>(config).Build())
+            //{
+            //    try
+            //    {
+            //        var dr = await p.ProduceAsync("myTopicName", new Message<Null, string> { Value = "user" });
+            //        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+            //    }
+            //    catch (ProduceException<Null, string> e)
+            //    {
+            //        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            //    }
+            //}
+
+            // if (args.Length != 3 && args.Length != 4) { PrintUsage(); }
+
+            var mode = "consume";
+            var topic = "users";
+            var configPath = "";
+
+            var certDir = @"C:\Users\Karunakaran\ssl\cacert.pem";
+
+            var config = await LoadConfig(configPath, certDir);
+
+            switch (mode)
+            {
+                case "produce":
+                    await CreateTopicMaybe(topic, 1, 3, config);
+                    Produce(topic, config);
+                    break;
+                case "consume":
+                    Consume(topic, config);
+                    break;
+                default:
+                    PrintUsage();
+                    break;
+            }
+        }
+    }
+}
